68d9c1011054b2896e4b57d9fd9118958cf3ae2b
ADILSON MARTINS DA SILVA <adilson.msilva4@senacsp.edu.br>
2023-10-16 22:18:09 -0300

    Commit_16-10-23

---
+++ DEV_FULLSTACK_DATA_JAVA/_js_aulas/05_00/js/scripts.js
@@ -0,0 +1,29 @@
+let Dig_Mes = parseFloat(prompt("Digite o numero de 1 à 12 correspondente ao MES"))
+
+if (Dig_Mes === 1) {
+    console.log("Janeiro_2023");
+} else {
+    console.log("Fevereiro_2023");
+} else if (Dig_Mes === 2) {
+    console.log("Março");
+}else if (Dig_Mes === 3) {
+    console.log("Abril");
+}else if (Dig_Mes === 4) {
+    console.log("Maio");
+}else if (Dig_Mes === 5) {
+    console.log("Junho");
+}else if (Dig_Mes === 6) {
+    console.log("Julho");
+}else if (Dig_Mes === 7) {
+    console.log("Agosto");
+}else if (Dig_Mes === 8) {
+    console.log("Otubro");
+}else if (Dig_Mes === 9) {
+    console.log("Fevereiro_202");
+}else if (Dig_Mes === 10) {
+    console.log("Fevereiro_202");
+}else if (Dig_Mes === 11) {
+    console.log("Fevereiro_202");
+}else if (Dig_Mes === 12) {
+    console.log("Fevereiro_202");
+}
