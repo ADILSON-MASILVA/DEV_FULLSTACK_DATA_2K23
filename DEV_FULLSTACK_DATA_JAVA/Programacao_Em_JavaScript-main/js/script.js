41e7bb6cd213dc74483d6146b7d34bf6f37bb8e8
ADILSON MARTINS DA SILVA <adilson.msilva4@senacsp.edu.br>
2023-10-18 20:03:05 -0300

    Commit_16-10-23

---
+++ DEV_FULLSTACK_DATA_JAVA/Programacao_Em_JavaScript-main/js/script.js
@@ -0,0 +1,28 @@
+let numero = parseInt(prompt("Digite um numero"))
+switch(numero) {
+    case 1:
+    console.log("jan")
+    case 2:
+    console.log("Fev")
+    case 3:
+    console.log("Mar")
+    case 4:
+    console.log("Abr")
+    case 5:
+    console.log("Mai")
+    case 6:
+    console.log("jun")
+    case 7:
+    console.log("jul")
+    case 8:
+    console.log("Ago")
+    case 9:
+    console.log("Set")
+    case 10:
+    console.log("Out")
+    case 11:
+    console.log("Nov")
+    case 12:
+    console.log("Dez")
+
+}
