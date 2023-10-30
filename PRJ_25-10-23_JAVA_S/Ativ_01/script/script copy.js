535770399a2b8227563473e71e50486be4239256
ADILSON MARTINS DA SILVA <adilson.msilva4@senacsp.edu.br>
2023-10-30 19:28:27 -0300

    Commit_20-10-23

---
+++ PRJ_25-10-23_JAVA_S/Ativ_01/script/script copy.js
@@ -0,0 +1,73 @@
+const lista = [1, 2, 3, 4, 5]
+
+console.log (lista)
+console.log(typeof lista)
+
+const itens = ["aluno", true, 2, 4.75]
+console.log(itens)
+
+const arr = ["a", "b", "c", "d"]
+console.log (arr)
+console.log (arr[0])
+console.log (arr[3])
+console.log (arr[10])
+
+const numbers = [5,12,4,22]
+console.log (numbers.length)
+
+const myName = "Rodox"
+console.log(myName.length)
+
+otherNumbers = [1, 2, 3]
+const allNumbers = myName.concat(otherNumbers)
+console.log (allNumbers)
+
+const text = "algum texto"
+console.log(text.toUpperCase())
+console.log(typeof text.toUpperCase())
+
+console.log(text.indexOf("t"))
+
+const person = {
+    name: "Rodox",
+    age: 37,
+    job: "programador"
+}
+console.log(person)
+console.log(person.name)
+console.log(person.age)
+console.log(person.job)
+
+console.log(typeof person)
+console.log(typeof person.name)
+console.log(typeof person.age)
+console.log(typeof person.job)
+
+const car = {
+    engine: 2.0;
+    brand: "VW",
+    model: "Tiguan",
+    km: 2000,
+}
+
+console.log(car)
+car.doors = 4;
+console.log(car)
+
+delete car.km
+console.log(car)
+
+const oj ={
+    a: "teste",
+    b: true,
+}
+
+console.log(obj instanceof object)
+
+const obj2 = {
+    c: [],
+}
+
+console.log(obj2)
+
+
---
+++ PRJ_25-10-23_JAVA_S/Ativ_01/script/script.js
@@ -0,0 +1,82 @@
+/** const lista = [1, 2, 3, 4, 5]
+
+console.log (lista)
+console.log(typeof lista)
+
+const itens = ["aluno", true, 2, 4.75]
+console.log(itens)
+
+const arr = ["a", "b", "c", "d"]
+console.log (arr)
+console.log (arr[0])
+console.log (arr[3])
+console.log (arr[10])
+
+/** 00 relacao de orientacao a objetos 
+const obj = {
+    a: "teste",
+    b: true,
+}
+
+console.log(obj instanceof Object)
+
+const obj2 =  {
+    c: [],
+}
+console.log(obj2 instanceof Object)
+
+Object.assign(obj2, obj)
+
+console.log(object.keys(obj))
+console.log(object.keys(obj2))
+console.log(object.keys(person))
+
+01  relacao de orientacao a objetos 
+
+const users = ["Rodox", "Pedro", "Miguel", "Solange", "Epscopaulo"]
+
+console.log (users)
+
+for(let i = 0; i<users.length;i++){
+console.log (`Listando Usuarios ${users[i]}`)
+}
+
+/** 02  relacao de orientacao a objetos 
+const ComMent = ["-------------------------------------------------------------"]
+
+console.log (ComMent)
+
+const array = ["a", "b", "c"]
+array.push("d")
+console.log(array)
+console.log(array.length)
+
+array.pop()
+const intemRemovido = array.pop()
+console.log(itemRemovido)
+console.log(array)
+console.log(array.length)
+
+array.push("da", "para", "inserir", "varios")
+
+console.log(array)
+const letters = ["a", "b", "c"]
+const letter = letters.shift()
+
+console.log(letters)
+console.log(letter) */
+/** 03  relacao de orientacao a objetos 
+
+letters.unshift("2", "x", "y")
+letters.unshift("p")
+console.log(letters)
+
+ /** 04  relacao de orientacao a objetos */
+const myElements = ["morango", "maça", "abacate", "Pera", "abacate"]
+
+console.log(myElements.indexOf("maça"))
+console.log(myElements.indexOf("abacate"))
+console.log(myElements[2])
+console.log(myElements[myElements.indexOf("abacate")])
+console.log(myElements.lastIndexOf("abacate"))
+/** 05  relacao de orientacao a objetos */
